426539273209d7e78afc68fc2af5e6b4f7899a0d
Justin Baiyu Wang <justin.wang1ab@gmail.com>
2023-08-08 20:53:58 -0400

    Use environment variables

--- back/database.js
+++ back/database.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 const MySQLStore = require('express-mysql-session')(session);
 const bcrypt = require('bcrypt');
 const fs = require('fs');
+require('dotenv').config();
 
 // Sets up a connection to AquaRecord database to store sessions
 const cookieStore = new MySQLStore({
@@ -11,8 +12,8 @@ const cookieStore = new MySQLStore({
     port: "3306",
     user: "azureuser",
     database: "AquaRecord",
-    password: 'StayHydrated!H20',
-    ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")}
+    password: process.env.PASSWRORD,
+    ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem", 'utf8')}
 });
 
 // Sets up a connection to AquaRecord to store user data
@@ -20,13 +21,15 @@ const aquaRecordDB = mysql.createConnection({
     host: "aquarecordserver.mysql.database.azure.com",
     user: "azureuser",
     database: "AquaRecord",
-    password: 'StayHydrated!H20',
+    password: process.env.PASSWORD,
     multipleStatements: true,  
-    ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")}
+    ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem", 'utf8')}
 });
 
 // Function for verifying the login credentials of the user
 const verifyUser = (username, pwd, done) => { 
+    console.log(process.env.PASSWORD)
+
     aquaRecordDB.query(`SELECT * FROM Users WHERE Username = '${username}'`, async function (error, results, fields) {
         if (error){ 
             return done(error);
@@ -34,6 +37,7 @@ const verifyUser = (username, pwd, done) => {
 
         if (results.length == 0)
             return done(null, false, { message: "No user with that username." });
+
         try {
             if (await bcrypt.compare(pwd, results[0].Password)) {
                 const user = {
